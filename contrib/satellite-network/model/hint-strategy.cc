3a791217991bae1efb2841e0dccb337f4e90156a
markverick <stheera@g.ucla.edu>
2023-10-23 21:37:24 +0000

    add hint forwarding strategy

---
+++ contrib/satellite-network/model/hint-strategy.cc
@@ -0,0 +1,204 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2014-2021,  Regents of the University of California,
+ *                           Arizona Board of Regents,
+ *                           Colorado State University,
+ *                           University Pierre & Marie Curie, Sorbonne University,
+ *                           Washington University in St. Louis,
+ *                           Beijing Institute of Technology,
+ *                           The University of Memphis.
+ *
+ * This file is part of NFD (Named Data Networking Forwarding Daemon).
+ * See AUTHORS.md for complete list of NFD authors and contributors.
+ *
+ * NFD is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * NFD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * NFD, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "hint-strategy.h"
+#include "common/global.hpp"
+#include "ns3/ndnSIM/NFD/daemon/fw/algorithm.hpp"
+#include "ns3/ndnSIM/NFD/daemon/common/logger.hpp"
+
+namespace nfd {
+namespace fw {
+
+NFD_LOG_INIT(HintStrategy);
+NFD_REGISTER_STRATEGY(HintStrategy);
+
+const time::milliseconds HintStrategy::RETX_SUPPRESSION_INITIAL(10);
+const time::milliseconds HintStrategy::RETX_SUPPRESSION_MAX(250);
+
+HintStrategy::HintStrategy(Forwarder& forwarder, const Name& name)
+  : Strategy(forwarder)
+  , m_retxSuppression(RETX_SUPPRESSION_INITIAL,
+                      RetxSuppressionExponential::DEFAULT_MULTIPLIER,
+                      RETX_SUPPRESSION_MAX)
+{
+  ParsedInstanceName parsed = parseInstanceName(name);
+  if (!parsed.parameters.empty()) {
+    NDN_THROW(std::invalid_argument("HintStrategy does not accept parameters"));
+  }
+  if (parsed.version && *parsed.version != getStrategyName()[-1].toVersion()) {
+    NDN_THROW(std::invalid_argument(
+      "HintStrategy does not support version " + to_string(*parsed.version)));
+  }
+  this->setInstanceName(makeInstanceName(name, getStrategyName()));
+}
+
+const Name&
+HintStrategy::getStrategyName()
+{
+  static const auto strategyName = Name("/localhost/nfd/strategy/hint").appendVersion(0);
+  return strategyName;
+}
+
+void HintStrategy::sendNackOrForward(const Interest& interest, const FaceEndpoint& faceEndpoint, const shared_ptr<pit::Entry>& pitEntry) {
+  // Don't use a forwarding hint when handling NACK
+  const fib::Entry* fibEntry = &this->lookupFib(interest);
+
+  const fib::NextHopList& nexthops = fibEntry->getNextHops();
+  auto it = nexthops.end();
+
+  // Check if there's any eligible next-hop
+  it = std::find_if(nexthops.begin(), nexthops.end(), [&] (const auto& nexthop) {
+    return isNextHopEligible(faceEndpoint.face, interest, nexthop, pitEntry);
+  });
+  // The only hop is the incoming face, send nack
+  if (it == nexthops.end()) {
+    NFD_LOG_DEBUG(interest << " from=" << faceEndpoint << " noNextHop");
+    std::cout << "nack," << interest.getName().getSubName(-1) << "," << ns3::Simulator::Now().GetMicroSeconds() << std::endl;
+    // std::cout << "OG NACK SENT: " << interest << " from=" << ingress << " noNextHop" << std::endl;
+    lp::NackHeader nackHeader;
+    nackHeader.setReason(lp::NackReason::NO_ROUTE);
+    this->sendNack(nackHeader, faceEndpoint.face, pitEntry);
+    // pitEntry->insertOrUpdateInRecord(ingress.face, interest);
+    // this->rejectPendingInterest(pitEntry);
+    return;
+  }
+
+  // Found eligible face, simply forward
+  Face& outFace = it->getFace();
+  NFD_LOG_DEBUG(interest << " from=" << faceEndpoint << " newPitEntry-to=" << outFace.getId());
+  this->sendInterest(interest, outFace, pitEntry);
+  return;
+}
+
+void HintStrategy::sendNackOrForward(const Interest& interest, const shared_ptr<pit::Entry>& pitEntry) {
+  const fib::Entry& fibEntry = this->lookupFib(interest);
+
+  const fib::NextHopList& nexthops = fibEntry.getNextHops();
+  auto it = nexthops.end();
+
+  // Check if there's any eligible next-hop
+  // We don't know the ingress face (from the interest) that triggers nack,
+  // so we look up the pit in records
+  it = std::find_if(nexthops.begin(), nexthops.end(), [&] (const auto& nexthop) {
+    const Face& outFace = nexthop.getFace();
+    // Return true if the new next hop does not match any in records of the PIT
+    for (auto& inFace : pitEntry->getInRecords()) {
+      if (outFace.getId() == inFace.getFace().getId())
+        return false;
+    }
+    return true;
+  });
+  // If the next-hop is not found or is the ingress face, propagate the nack
+  if (it == nexthops.end()) {
+    // std::cout << "PROP NACK SENT: " << nack.getInterest().getName() << std::endl;
+    NFD_LOG_DEBUG(interest << " noNextHop");
+    while (pitEntry->hasInRecords()) {
+      lp::NackHeader nackHeader;
+      nackHeader.setReason(lp::NackReason::NO_ROUTE);
+      this->sendNack(nackHeader, pitEntry->getInRecords().begin()->getFace(), pitEntry);
+    }
+    // this->rejectPendingInterest(pitEntry);
+  } else {
+    // Attempt to retransmit
+    Face& outFace = it->getFace();
+    // Re-insert the out record
+    pitEntry->insertOrUpdateOutRecord(outFace, interest);
+    std::cout << "retrans," << interest.getName().getSubName(-1) << "," << ns3::Simulator::Now().GetMicroSeconds() << std::endl;
+    // std::cout << "RETRANS SENT: " << ingress.face.getId() << " -> " << outFace.getId() << " of " << interest.getInterestLifetime() << std::endl;
+    NFD_LOG_DEBUG(interest << " retrans, newPitEntry-to=" << outFace.getId());
+    // Reset the expiry timer
+    this->setExpiryTimer(pitEntry, time::milliseconds(2000));
+    this->sendInterest(interest, outFace, pitEntry);
+  }
+}
+
+void
+HintStrategy::afterReceiveInterest(const Interest& interest, const FaceEndpoint& ingress,
+                                        const shared_ptr<pit::Entry>& pitEntry)
+{
+  RetxSuppressionResult suppression = m_retxSuppression.decidePerPitEntry(*pitEntry);
+  if (suppression == RetxSuppressionResult::SUPPRESS) {
+    NFD_LOG_DEBUG(interest << " from=" << ingress << " suppressed");
+    return;
+  }
+
+  if (suppression == RetxSuppressionResult::NEW) {
+    // forward to nexthop with lowest cost except downstream, or send nack if no eligible path
+    // std::cout << "SENT" << std::endl;
+    sendNackOrForward(interest, ingress, pitEntry);
+    return;
+  }
+
+  const fib::Entry& fibEntry = this->lookupFib(*pitEntry);
+  const fib::NextHopList& nexthops = fibEntry.getNextHops();
+  auto it = nexthops.end();
+
+  // find an unused upstream with lowest cost except downstream
+  it = std::find_if(nexthops.begin(), nexthops.end(),
+                    [&, now = time::steady_clock::now()] (const auto& nexthop) {
+                      return isNextHopEligible(ingress.face, interest, nexthop, pitEntry, true, now);
+                    });
+
+  if (it != nexthops.end()) {
+    Face& outFace = it->getFace();
+    this->sendInterest(interest, outFace, pitEntry);
+    NFD_LOG_DEBUG(interest << " from=" << ingress << " retransmit-unused-to=" << outFace.getId());
+    return;
+  }
+
+  // find an eligible upstream that is used earliest
+  it = findEligibleNextHopWithEarliestOutRecord(ingress.face, interest, nexthops, pitEntry);
+  if (it == nexthops.end()) {
+    NFD_LOG_DEBUG(interest << " from=" << ingress << " retransmitNoNextHop");
+  }
+  else {
+    Face& outFace = it->getFace();
+    this->sendInterest(interest, outFace, pitEntry);
+    NFD_LOG_DEBUG(interest << " from=" << ingress << " retransmit-retry-to=" << outFace.getId());
+  }
+}
+
+void
+HintStrategy::afterReceiveNack(const lp::Nack& nack, const FaceEndpoint& ingress,
+                                    const shared_ptr<pit::Entry>& pitEntry)
+{
+  // std::cout << "NACK RECEIVED: " << nack.getInterest().getName() << std::endl;
+
+  // Try to retransmit or backtrack to retransmit
+  // We don't know which inFace triggers this nack because they are all aggregated
+  if (nack.getReason() == lp::NackReason::NO_ROUTE) {
+    // Strip the forwarding hint
+    if (!nack.getInterest().getForwardingHint().empty()) {
+      Interest interestNoHint = Interest(ndn::Name(nack.getInterest().getName()), time::milliseconds(2000));
+      interestNoHint.setForwardingHint({});
+      sendNackOrForward(interestNoHint, pitEntry);
+    } else {
+      sendNackOrForward(nack.getInterest(), pitEntry);
+    }
+  }
+}
+
+} // namespace fw
+} // namespace nfd
---
+++ scenarios/runs/a_b_test_hint.cc
@@ -0,0 +1,105 @@
+// 3nodes_test.cpp
+#include "../ndn-sat-simulator.h"
+#include "ns3/basic-simulation.h"
+#include <sys/resource.h>
+
+namespace ns3 {
+
+void sendInterestWithHint(Ptr<Node> gsNode, string prefix, string hintPrefix) {
+  // NS_ASSERT_MSG(gsNode->GetId() < m_satellites.size(), "Ground station ID is invalid");
+  // Create a name with sequence
+  int seq = 0;
+  shared_ptr<ndn::Name> nameWithSequence = make_shared<ndn::Name>(prefix);
+  nameWithSequence->appendSequenceNumber(seq);
+  // Create an interest with the given name
+  shared_ptr<ndn::Interest> interest = make_shared<ndn::Interest>();
+  auto rand = CreateObject<UniformRandomVariable>();
+  interest->setNonce(rand->GetValue(0, std::numeric_limits<uint32_t>::max()));
+  interest->setName(*nameWithSequence);
+  interest->setCanBePrefix(false);
+  ndn::time::milliseconds interestLifeTime(2000);
+  interest->setInterestLifetime(interestLifeTime);
+  vector<ndn::Name> del;
+  del.emplace_back(ndn::Name(hintPrefix));
+  interest->setForwardingHint(del);
+  Ptr<ns3::ndn::L3Protocol> ndn = gsNode->GetObject<ns3::ndn::L3Protocol>();
+  ndn->getFaceByNetDevice(gsNode->GetDevice(0))->sendInterest(*interest);
+  std::cout << "interest sent" << std::endl;
+}
+
+class ScenarioSim : public NDNSatSimulator {
+public:
+  using NDNSatSimulator::NDNSatSimulator;
+  void Run() {
+    // Choosing forwarding strategy
+    std::cout << "  > Installing forwarding strategy" << std::endl;
+    ndn::StrategyChoiceHelper::Install(m_allNodes, "/", "/localhost/nfd/strategy/hint");
+    std::string prefix = "/leo/";
+    Ptr<Node> node1 = m_allNodes.Get(m_node1_id);
+    Ptr<Node> node2 = m_allNodes.Get(m_node2_id);
+    std::string prefix1 = prefix + to_string(m_node1_id);
+    std::string prefix2 = prefix + to_string(m_node2_id);
+    std::string hintPrefix = prefix + "1500"; //redirect to sat 1500
+    m_prefix = prefix2;
+    // cout << "PREFIX: " << prefix2 << endl;
+    // Consumer
+    ns3::Simulator::Schedule(Seconds(5), &sendInterestWithHint, node1, m_prefix, hintPrefix);
+    // ndn::AppHelper consumerHelper("ns3::ndn::ConsumerPing");
+    // // Consumer will request /leo/0, /leo/1, ...
+    // consumerHelper.SetPrefix(m_prefix);
+    // consumerHelper.SetAttribute("Frequency", StringValue("0.1"));
+    // consumerHelper.SetAttribute("RetxTimer", StringValue("10000s"));
+    // consumerHelper.Install(node1).Start(Seconds(0)); // first node
+
+    // Producer
+    ndn::AppHelper producerHelper("ns3::ndn::Producer");
+    // Producer will reply to all requests starting with prefix
+    producerHelper.SetPrefix(m_prefix);
+    producerHelper.SetAttribute("PayloadSize", StringValue("0"));
+    producerHelper.Install(node2).Start(Seconds(0)); // last node
+
+    cout << "Setting up FIB schedules..."  << endl;
+
+    ImportDynamicStateSat(m_allNodes, m_satellite_network_routes_dir, 0, true);
+
+    cout << "Starting the simulation"  << endl;
+    Simulator::Stop(Seconds(200));
+    // int start_index = -1;
+    // for (int i = m_satellite_network_dir.size() - 1; i >= 0; i--) {
+    //   char c = m_satellite_network_dir[i];
+    //   if (c == '/') {
+    //     start_index = i + 1;
+    //     break;
+    //   }
+    // }
+    // string run_dir = m_satellite_network_dir.substr(start_index);
+    // cout << "experiments/a_b/runs/" + m_name + "/app-delays-trace.txt" << endl;
+    ndn::AppDelayTracer::InstallAll("experiments/a_b/runs/" + m_name + "/app-delays-trace.txt");
+    Simulator::Run();
+    Simulator::Destroy();
+  }
+};
+}
+// NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=a_b_starlink_BJ_NY_ping |& tee -a logs/ndn_ping_loss_01.txt
+
+// NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=a_b_starlink_BJ_NY_ping |& tee -a logs/ping-multicast/instant-remove.txt
+int
+main(int argc, char* argv[])
+{
+  // No buffering of printf
+  setbuf(stdout, nullptr);
+  // Retrieve run directory
+  ns3::CommandLine cmd;
+  std::string run_dir = "";
+  cmd.Usage("Usage: ./waf --run=\"run --run_dir='<path/to/run/directory>'\"");
+  cmd.AddValue("run_dir",  "Run directory", run_dir);
+  cmd.Parse(argc, argv);
+  if (run_dir.compare("") == 0) {
+      printf("Usage: ./waf --run=\"run --run_dir='<path/to/run/directory>'\"");
+      return 0;
+  }
+  string ns3_config = "experiments/a_b/runs/" + run_dir + "/config_ns3.properties";
+  ns3::ScenarioSim sim = ns3::ScenarioSim(ns3_config);
+  sim.Run();
+  return 0;
+}
